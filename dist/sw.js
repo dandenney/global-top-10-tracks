0f908d7023d1f504f04248ac38fbbf0876023a81
Dan Denney <dd.subscriptions@gmail.com>
2016-12-08 20:46:59 -0500

    feat: Update service worker cache assets

--- dist/sw.js
+++ dist/sw.js
@@ -1,45 +1,58 @@
-var CACHE = 'cache-and-update';
+var CACHE_NAME = 'global-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/app.css',
+  '/js/app.js',
+  '/data/global-top-10-tracks.json'
+];
 
-self.addEventListener('install', function(evt) {
-
-  console.log('The service worker is being installed');
-  evt.waitUntil(precache());
-
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', function(evt) {
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
 
-  console.log('The service worker is serving the asset.');
-  evt.respondWith(fromCache(evt.request));
-  evt.waitUntil(update(evt.request));
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
 
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
-
-function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll([
-      '/data/global-top-10-tracks.json',
-      '/images/6fujklziTHa8uoM5OQSfIo.jpeg',
-      '/images/7MXVkk9YMctZqd1Srtv4MB.jpeg',
-      '/js/app.js',
-      '/styles/app.css',
-      '/index.html'
-    ]);
-  });
-}
-
-function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
-}
-
-function update(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
-    });
-  });
-}
