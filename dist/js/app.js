373e1862bcd3d9eb83a58eea15cc8907a226491e
Dan Denney <dd.subscriptions@gmail.com>
2016-12-09 20:30:28 -0500

    refactor: Organize the functions into data retrieval and rendering
    Fixes: #3
    
    There's lots more to do to make this better, but this gets it to a
    starting point for the next steps. At its current state, it is a
    functional PWA.

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,26 +1,12 @@
 // *************************************
 //
-//   File Name
-//   -> Description
+//   Track Life
+//   -> App JS
 //
 // *************************************
 
   // -------------------------------------
-  //   Private Variables
-  // -------------------------------------
-
-  // ...
-
-  // -------------------------------------
-  //   Initialize
-  // -------------------------------------
-
-  // -------------------------------------
-  //   Set Event Handlers
-  // -------------------------------------
-
-  // -------------------------------------
-  //   Service Worker
+  //   Register a Service Worker
   // -------------------------------------
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
@@ -33,86 +19,117 @@
       });
     });
   }
+
   // -------------------------------------
-  //   Usage
+  //   Fetch JSON
   // -------------------------------------
+
   // Get a URL with fetch
   function get(url) {
     return fetch(url, {
       method: 'get',
     });
   }
+
   // Get JSON from a URL
   function getJSON(url) {
     return get(url).then(response => {
       return response.json();
     });
   }
+
   // Output JSON into the dom
   function outputJSON() {
     getJSON('data/global-top-10-tracks.json')
-    .then(response => {
-      // Variables
-      const tracks = response.tracks;
-      // Loop over tracks
-      tracks.forEach((track, i) => {
-        // Set variables from JSON keys
-        const ID = track.trackID;
-        const name = track.trackName;
-        const artist = track.trackArtist;
-        // Output from the JSON keys using template strings, woohoo!
-        document.querySelector('#all').innerHTML += `
-          <article>
-            <img src="images/${ID}.jpeg" />
-            <h3><a class='track-link' data-track='${i}' href='#track'>${name}</a></h3>
-            <h4>${artist}</h4>
-          </article>
-        `;
-      });
-      // Variables
-      const trackLinks = document.querySelectorAll('.track-link');
-      // Loop over track data
-      trackLinks.forEach(trackLink => {
-        // Listen for clicks to tracks
-        trackLink.addEventListener('click', function () {
-          document.querySelector('#track-positions').innerHTML = '';
-          document.querySelector('#track-streams').innerHTML = '';
-          // Variables
-          const index = this.getAttribute('data-track');
-          const name = response.tracks[index].trackName;
-          const artist = response.tracks[index].trackArtist;
-          const ID = response.tracks[index].trackID;
-          const trackPositions = response.tracks[index].trackPositions;
-          const trackStreams = response.tracks[index].trackStreams;
-          // Output track meta information
-          document.querySelector('#track-meta').innerHTML = `
-            <img src='images/${ID}.jpeg' />
-            <h1>${name}</h1>
-            <h2>${artist}</h2>
-          `;
-          // Output track positions
-          trackPositions.forEach(trackPosition => {
-            const date = trackPosition.positionDate;
-            const rank = trackPosition.positionRank;
-            document.querySelector('#track-positions').innerHTML += `
-              <li>${date}: ${rank}</li>
-            `;
-          });
-
-          // Output track positions
-          trackStreams.forEach(trackStream => {
-            const date = trackStream.streamDate;
-            const count = trackStream.streamCount;
-            document.querySelector('#track-streams').innerHTML += `
-              <li>${date}: ${count}</li>
-            `;
-          });
-        });
-      });
-    })
-    .catch(error => {
-      console.log(error);
-    });
+      // If retrieving JSON is succesful
+      .then(response => renderTracks(response))
+      // If retrieving JSON fails
+      .catch(error => console.log(error));
   }
+
+  // -------------------------------------
+  //   Render Tracks
+  // -------------------------------------
+
+  function renderTracks(response) {
+
+    // -------------------------------------
+    //   Render All Tracks
+    // -------------------------------------
+
+    // Variables
+    const tracks = response.tracks;
+    // Loop over tracks
+    tracks.forEach((track, i) => {
+      // Set variables from JSON keys
+      const ID = track.trackID;
+      const name = track.trackName;
+      const artist = track.trackArtist;
+      // Output from the JSON keys using template strings, woohoo!
+      document.querySelector('#all').innerHTML += `
+        <article>
+          <img src="images/${ID}.jpeg" />
+          <h3><a class='track-link' data-track='${i}' href='#track'>${name}</a></h3>
+          <h4>${artist}</h4>
+        </article>
+      `;
+    });
+
+    // -------------------------------------
+    //   Render Single Tracks
+    // -------------------------------------
+
+    // Variables
+    const trackLinks = document.querySelectorAll('.track-link');
+
+    // Loop over track data
+    trackLinks.forEach(trackLink => {
+
+      // Listen for clicks to tracks
+      trackLink.addEventListener('click', function () {
+        document.querySelector('#track-positions').innerHTML = '';
+        document.querySelector('#track-streams').innerHTML = '';
+
+        // Variables
+        const index = this.getAttribute('data-track');
+        const name = response.tracks[index].trackName;
+        const artist = response.tracks[index].trackArtist;
+        const ID = response.tracks[index].trackID;
+        const trackPositions = response.tracks[index].trackPositions;
+        const trackStreams = response.tracks[index].trackStreams;
+
+        // Output track meta information
+        document.querySelector('#track-meta').innerHTML = `
+          <img src='images/${ID}.jpeg' />
+          <h1>${name}</h1>
+          <h2>${artist}</h2>
+        `;
+
+        // Output track positions
+        trackPositions.forEach(trackPosition => {
+          const date = trackPosition.positionDate;
+          const rank = trackPosition.positionRank;
+          document.querySelector('#track-positions').innerHTML += `
+            <li>${date}: ${rank}</li>
+          `;
+        });
+
+        // Output track positions
+        trackStreams.forEach(trackStream => {
+          const date = trackStream.streamDate;
+          const count = trackStream.streamCount;
+          document.querySelector('#track-streams').innerHTML += `
+            <li>${date}: ${count}</li>
+          `;
+        });
+
+      });
+    });
+
+  }
+
+  // -------------------------------------
+  //   Initialize
+  // -------------------------------------
 
   outputJSON();
